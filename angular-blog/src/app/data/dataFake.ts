838302aab39b577773607fdbb5e28aad347c28eb
Julio Souza <visoboti@gmail.com>
2023-10-04 22:11:52 -0300

    finalizando rotas

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+    {
+        "id":'1',
+        "title":"Ichigo Surge",
+        "description":"Ichigo falido e morando de aluguel",
+        'photoCover':"https://i0.wp.com/hitsite.com.br/wp-content/uploads/2022/10/bleach.jpg?fit=1200%2C675&ssl=1"
+    
+    },
+    {
+        "id":'2',
+        "title":"bora bill",
+        "description":"ahh lelek",
+        'photoCover':"https://sm.ign.com/ign_br/screenshot/default/ichigo-kurosaki_1xra.jpg"
+    
+    },
+    {
+        "id":'3',
+        "title":"bora bill",
+        "description":"ahh lelek",
+        'photoCover':"https://pbs.twimg.com/media/FrWR6eKakAIl8eV.jpg:large"
+    
+    }
+
+]
+
+
+
+
---
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit ,Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  @Input()
+  photoCover : string =""
+  @Input()
+  contentTitle : string ="Gotei 13"
+  @Input()
+  contentDescription : string =" Brabos "
+  
+  private id:string | null ='0 '
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value => 
+      this.id = value.get("id")
+      )
+      this.setValueToComponent(this.id)
+  }
+
+  setValueToComponent(id:string | null ){
+    const result = dataFake.filter( 
+      article => article.id === id)[0]
+    
+        this.contentTitle = result.title
+        this.contentDescription = result.description
+        this.photoCover = result.photoCover
+      
+  }
+}
