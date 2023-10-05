391d816661a87f160c4c6ae44938bd2df55eb0aa
Julio Souza <visoboti@gmail.com>
2023-10-05 12:13:44 -0300

    tentando finalizar edeixar bonito

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -2,22 +2,50 @@ export const dataFake = [
     {
         "id":'1',
         "title":"Ichigo Surge",
-        "description":"Ichigo falido e morando de aluguel",
+        "description":"Ichigo falido e morando de aluguel No início da história, Ichigo é um adolescente com a capacidade de ver espíritos, mas ele não possui habilidades espirituais extraordinárias. No entanto, sua vida muda drasticamente quando ele encontra Rukia Kuchiki, uma shinigami, e acidentalmente obtém seus poderes após um encontro com um hollow. Nesta fase, ele só possui habilidades básicas de um shinigami.",
         'photoCover':"https://i0.wp.com/hitsite.com.br/wp-content/uploads/2022/10/bleach.jpg?fit=1200%2C675&ssl=1"
     
     },
     {
         "id":'2',
-        "title":"bora bill",
-        "description":"ahh lelek",
-        'photoCover':"https://sm.ign.com/ign_br/screenshot/default/ichigo-kurosaki_1xra.jpg"
+        "title":"Shikai e Bankai",
+        "description":"Após receber treinamento intensivo na Soul Society, Ichigo aprende a liberar o seu zanpakutou, Zangetsu, em sua forma Shikai (Zangetsu é uma katana negra e sua forma Shikai permite que ela se estenda). Posteriormente, ele alcança o Bankai, uma forma avançada de seu zanpakutou, que aumenta significativamente suas habilidades de combate.",
+        'photoCover':"https://i.pinimg.com/originals/74/ba/13/74ba1316b545dfbbe109d62d4c460c3e.jpg"
     
     },
     {
         "id":'3',
-        "title":"bora bill",
-        "description":"ahh lelek",
-        'photoCover':"https://pbs.twimg.com/media/FrWR6eKakAIl8eV.jpg:large"
+        "title":"Vizard",
+        "description":"Durante a saga dos Vizards, Ichigo adquire temporariamente poderes hollow e treina com outros Vizards para controlar essas habilidades. Isso permite que ele acesse uma forma híbrida de shinigami e hollow, melhorando ainda mais suas habilidades de combate.",
+        'photoCover':"https://c4.wallpaperflare.com/wallpaper/651/123/748/anime-bleach-character-guy-wallpaper-preview.jpg"
+    
+    },
+    {
+        "id":'4',
+        "title":"Dangai Training",
+        "description":"Após perder seus poderes shinigami, Ichigo realiza um treinamento intenso no Dangai (Caminho da Perdição) para recuperá-los. Durante esse período, ele adquire um novo Bankai chamado Tensa Zangetsu, que aumenta suas habilidades a um nível extremamente alto.",
+        'photoCover':"https://i.pinimg.com/originals/de/d7/00/ded7007ad9c1689c12d38081b7ea3633.jpg"
+    
+    },
+    {
+        "id":'5',
+        "title":"Fullbring",
+        "description":"Após perder seus poderes shinigami novamente, Ichigo desenvolve habilidades Fullbring, que são baseadas em objetos cotidianos. Ele adquire um Fullbring chamado Fullbring Eterno do Rei que lhe dá poderes especiais. No entanto, esses poderes são temporários.",
+        'photoCover':"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRivw6zf54VEBRIoTp1Nj0P-ABvmLBwbzcJaCcamVJBj9_Uu0mXLEdEUE4XmZ4y7o9jGPc&usqp=CAU"
+    
+    },
+    {
+        "id":'6',
+        "title":"Retorno dos Poderes Shinigami:",
+        "description":"Com a ajuda de Xcution e Urahara, Ichigo recupera seus poderes shinigami e continua a desenvolvê-los, tornando-se ainda mais forte do que antes.",
+        'photoCover':"https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc/wp-content/uploads/2023/01/legiao_qvTgI93xcVJ0.jpg.webp"
+    
+    },
+    {
+        "id":'7',
+        "title":"Forma Final:",
+        "description":"No arco final de Bleach, Ichigo alcança uma forma final chamada Fullbring Bankai ou Bankai Final. Essa forma combina todas as suas evoluções anteriores e representa seu pináculo de poder. Nesta forma, ele é extraordinariamente poderoso.",
+        'photoCover':"https://trecobox.com.br/wp-content/uploads/2019/01/bleach-ichigo-forma-suprema.jpg.webp"
     
     }
 
